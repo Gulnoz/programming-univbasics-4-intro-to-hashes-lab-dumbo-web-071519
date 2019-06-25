e54b20a3cca58bd2418f46adcee28b2d9899e7ea
fortuitous-counter-8979 <gmcharmy@gmail.com>
2019-06-25 00:42:33 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,18 +15,22 @@ end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
+  return {id: 7}
 end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
+   return {key=>value}
 end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
+  return hash[key]
 end
 
 def update_counting_hash(hash, key)
   # given a hash an a key as parameters, return an updated hash
   # if the provided key is not present in the hash, add it and assign it to the value of 1
   # if the provided key is present, increment its value by 1
+  return hash[key] ? hash[key]+=1, hash[key]=1
 end
